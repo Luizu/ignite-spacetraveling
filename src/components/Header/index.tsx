e2ab0c7d674f4f6935a53509cf17e911b5c8714d
luizu <luizucastilho@gmail.com>
2021-04-02 14:25:14 -0300

    Chore: Create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.container}>
+      <div>
+        <img src="/images/logo.svg" alt="Spacetraveling logo" />
+      </div>
+    </header>
+  );
 }
