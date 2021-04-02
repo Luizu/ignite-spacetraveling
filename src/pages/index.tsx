52513c3c5e8406c041efba6b977f1ce2c3275fc1
luizu <luizucastilho@gmail.com>
2021-04-02 15:47:36 -0300

    Chore: Create home page and get data from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
 import { GetStaticProps } from 'next';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import { RichText } from 'prismic-dom';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +31,67 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | Spacetraveling</title>
+      </Head>
+      <Header />
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main className={commonStyles.container}>
+        <div className={`${commonStyles.contentContainer} ${styles.content}`}>
+          {postsPagination.results.map(post => {
+            return (
+              <a key={post.uid}>
+                <h1>{post.data.title}</h1>
+                <p>{post.data.subtitle}</p>
+                <time>
+                  <FiCalendar /> {post.first_publication_date}
+                </time>
+                <span>
+                  <FiUser /> {post.data.author}
+                </span>
+              </a>
+            );
+          })}
+        </div>
+      </main>
+    </>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {}
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.slugs,
+      data: {
+        title: RichText.asText(post.data.title),
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+      first_publication_date: format(
+        new Date(post.last_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        results: posts,
+      },
+    },
+    revalidate: 60 * 30, // 30 minutes
+  };
+};
