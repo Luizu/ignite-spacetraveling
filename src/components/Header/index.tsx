674c40010dc151a000be61f2ce1002858e0d9f2b
luizu <luizucastilho@gmail.com>
2021-04-02 17:18:19 -0300

    Chore: Add home navigation on logo click

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.container}>
-      <div>
-        <img src="/images/logo.svg" alt="Spacetraveling logo" />
-      </div>
+      <Link href="/">
+        <a>
+          <img src="/images/logo.svg" alt="Spacetraveling logo" />
+        </a>
+      </Link>
     </header>
   );
 }
